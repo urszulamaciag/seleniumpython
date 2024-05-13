2190aa8dd1cff91893b10648bdb3e9336f6317db
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-13 21:59:04 +0200

    may05

--- tests/test_duckduckgo.py
+++ tests/test_duckduckgo.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -10,9 +8,10 @@ def test_searching_in_duckduckgo(chrome_browser):
     chrome_browser.get('https://duckduckgo.com')
     chrome_browser.find_element(By.CSS_SELECTOR, '#searchbox_input').send_keys('4testers')
     chrome_browser.find_element(By.CSS_SELECTOR, '[aria-label=Search]').click()
-    title_elements = chrome_browser.find_elements(By.CSS_SELECTOR, '[data-testid=result-title-a] span')
-
-    time.sleep(2)
+    title_selector = (By.CSS_SELECTOR, '[data-testid=result-title-a] span')
+    wait = WebDriverWait(chrome_browser, 10)
+    wait.until(lambda x: len(chrome_browser.find_elements(*title_selector)) == 10)
+    title_elements = chrome_browser.find_elements(*title_selector)
     assert check_text_presence(title_elements, '4_testers Automaty')
 
 
--- tests/test_test_arena.py
+++ tests/test_test_arena.py
@@ -1,16 +1,43 @@
+import pytest
+import random
+import string
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 from fixtures.chrome import chrome_browser
 
+administrator_email = 'administrator@testarena.pl'
 
-def test_successful_login(chrome_browser):
-    administrator_email = 'administrator@testarena.pl'
+
+@pytest.fixture
+def browser(chrome_browser):
     chrome_browser.set_window_size(1920, 1080)
     chrome_browser.get('http://demo.testarena.pl/zaloguj')
-
     chrome_browser.find_element(By.CSS_SELECTOR, '#email').send_keys(administrator_email)
     chrome_browser.find_element(By.CSS_SELECTOR, '#password').send_keys('sumXQQ72$L')
     chrome_browser.find_element(By.CSS_SELECTOR, '#login').click()
+    yield chrome_browser
 
-    user_email = chrome_browser.find_element(By.CSS_SELECTOR, ".user-info small")
+
+def test_successful_login(browser):
+    user_email = browser.find_element(By.CSS_SELECTOR, ".user-info small")
     assert administrator_email == user_email.text
 
+
+def test_add_message(browser):
+    browser.find_element(By.CLASS_NAME, 'icon_mail').click()
+    wait = WebDriverWait(browser, 10)
+    text_input = (By.ID, 'j_msgContent')
+    wait.until(EC.element_to_be_clickable(text_input))
+    my_text = generate_random_string(10)
+    browser.find_element(*text_input).send_keys(my_text)
+    browser.find_element(By.ID, 'j_msgResponse-193').click()
+    wait.until(lambda x: browser.find_elements(By.CLASS_NAME, 'message_content_text')[-1].text == my_text)
+    assert browser.find_elements(By.CLASS_NAME, 'message_content_text')[-1].text == my_text
+
+
+def generate_random_string(length=10):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
